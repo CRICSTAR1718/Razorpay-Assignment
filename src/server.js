d6f6ab825f1ac7ba57963ae24413a01a6580a3df
Adeela Azeez <adeelaazeez9@gmail.com>
2026-06-25 14:13:31 +0530

    Bug fix: Update package.json to fix dependency issues

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const app = require('./app');
 
 
-const PORT = 'https://razorpay-assignment.onrender.com' || 7002;
+const PORT =7002;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
